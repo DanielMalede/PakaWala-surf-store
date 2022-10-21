f5cfb8b3738810d1bafb6f8b58c7d7cf0392b9a8
DanielMalede <danielmalede@gmail.com>
2022-10-22 02:24:15 +0300

    save

--- src/components/pages/GoogleMaps/GoogleMaps.jsx
+++ src/components/pages/GoogleMaps/GoogleMaps.jsx
@@ -1,7 +1,14 @@
 import "./GoogleMaps.css";
 import React from "react";
+import { useLoadScript } from '@react-google-maps/api';
+
 
 function template() {
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+})
+if (!isLoaded) return <div>Loading...</div>
   return (
     <div className="google-maps">
       <h1>GoogleMaps</h1>
