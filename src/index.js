5363afb3c4cc72e740885ac70f27adec22c87a17
DanielMalede <danielmalede@gmail.com>
2022-11-03 14:57:17 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App"; 
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import { SurfBoardsProvider } from "./context/SurfBoardContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,17 +13,21 @@ root.render(
   <React.StrictMode>
     <UserAuthContextProvider>
       <SurfBoardsProvider>
-      <Router>
-        <App />
-        <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
-      </Router>
+        <Router>
+          <App />
+          <link
+            href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            rel="stylesheet"
+          />
+        </Router>
       </SurfBoardsProvider>
     </UserAuthContextProvider>
   </React.StrictMode>
-  
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
