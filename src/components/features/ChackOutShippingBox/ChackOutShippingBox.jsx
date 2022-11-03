8e7d5b32edba63895a4a7d005ff780177ea89eb0
DanielMalede <danielmalede@gmail.com>
2022-11-03 14:59:44 +0200

    .

--- src/components/features/ChackOutShippingBox/ChackOutShippingBox.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ChackOutShippingBox() {
-  return (
-    <div>ChackOutShippingBox</div>
-  )
-}
--- src/components/features/index.js
+++ src/components/features/index.js
@@ -2,8 +2,7 @@ import Card from "./Card/Card";
 import CartItem from "./CartItem/CartItem";
 import CarouselPopUp from "./CarouselPopUp/CarouselPopUp";
 import ChackOutVisaBox from "./ChackOutVisaBox/ChackOutVisaBox";
-import ChackOutShippingBox from "./ChackOutShippingBox/ChackOutShippingBox";
 import ChackOutSummaryBox from "./ChackOutSummaryBox/ChackOutSummaryBox";
 import ChackOutCartBox from "./ChackOutCartBox/ChackOutCartBox";
 import SurfBoardCard from "./SurfBoardCard/SurfBoardCard";
-export{Card,CartItem,CarouselPopUp,ChackOutVisaBox,ChackOutShippingBox,ChackOutSummaryBox,ChackOutCartBox,SurfBoardCard}
+export{Card,CartItem,CarouselPopUp,ChackOutVisaBox,ChackOutSummaryBox,ChackOutCartBox,SurfBoardCard}
