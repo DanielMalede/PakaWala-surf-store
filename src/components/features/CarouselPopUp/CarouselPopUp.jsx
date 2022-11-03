99e2fcbe012cc8d4791a6d6e3c1b4f1b1c89806f
DanielMalede <danielmalede@gmail.com>
2022-11-03 14:58:22 +0200

    .

--- src/components/features/CarouselPopUp/CarouselPopUp.jsx
+++ src/components/features/CarouselPopUp/CarouselPopUp.jsx
@@ -1,24 +1,21 @@
 import "./CarouselPopUp.css";
-import React from 'react';
-import {
-  MDBCarousel,
-  MDBCarouselItem,
-} from 'mdb-react-ui-kit';
+import React from "react";
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 
-export default function CarouselPopUp({id,counter}) {
+export default function CarouselPopUp({ id, counter }) {
   return (
     <MDBCarousel showControls dealy={100}>
       <MDBCarouselItem
-        className='w-100 d-block'
+        className="w-100 d-block"
         itemId={1}
         src={`images/SurfBoards/${id}.webp`}
-        alt='...'
+        alt="..."
       />
       <MDBCarouselItem
-        className='w-100 d-block'
+        className="w-100 d-block"
         itemId={2}
         src={`images/SurfBoards/${counter}.webp`}
-        alt='...'
+        alt="..."
       />
     </MDBCarousel>
   );
