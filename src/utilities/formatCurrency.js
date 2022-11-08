41ee457be7eeb82354c0af021aa813875552d09f
DanielMalede <danielmalede@gmail.com>
2022-11-08 16:51:43 +0200

    .

--- src/utilities/formatCurrency.js
+++ src/utilities/formatCurrency.js
@@ -1,5 +1,7 @@
-const CURRENCY = new Intl.NumberFormat(undefined,
-{currency:"USD",style:"currency"})
+const CURRENCY = new Intl.NumberFormat(undefined, {
+  currency: "USD",
+  style: "currency",
+});
 export default function formatCurrency(number) {
-    return CURRENCY.format(number)
+  return CURRENCY.format(number);
 }
